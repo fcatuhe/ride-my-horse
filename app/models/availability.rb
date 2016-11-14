aeb2acdceaffaccd1967762c2a0ac4ec935a7bc9
Francois Catuhe <fcatuhe@gmail.com>
2016-11-14 16:51:56 +0100

    updated models

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,3 +1,5 @@
 class Availability < ApplicationRecord
   belongs_to :horse
+  validates :start_at, presence: true
+  validates :finish_at, presence: true
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,4 +1,11 @@
 class Horse < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  has_one :category
+  has_many :availabilities, dependent: :destroy
+  has_many :bookings
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :equipment, presence: true
+  validates :description, presence: true
 end
