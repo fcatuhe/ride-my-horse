ad7e9fc7310ec39a54592b36817b042ba936a197
Francois Catuhe <fcatuhe@gmail.com>
2016-11-15 21:42:04 +0100

    add validation on foreign keys in horse and bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :horse
+  validates :user, presence: true
+  validates :horse, presence: true
   validates :date, presence: true
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,10 +3,11 @@ class Horse < ApplicationRecord
   belongs_to :category
   has_many :availabilities, dependent: :destroy
   has_many :bookings
+  validates :user, presence: true
+  validates :category, presence: true
   validates :name, presence: true
   validates :price, presence: true
   validates :address, presence: true
-  validates :equipment, presence: true
   validates :description, presence: true
   has_attachment :photo
 
