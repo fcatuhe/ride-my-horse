a57325eb86ac70f1787d850e7dccb36156aea076
gabriel ecalle <gabriel.ecalle@googlemail.com>
2016-11-15 15:39:28 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Level.destroy_all
+Category.destroy_all
+User.destroy_all
+Horse.destroy_all
+
+
 Level.create({ name: 'Beginner' })
 Level.create({ name: 'Intermediate' })
 Level.create({ name: 'Advanced' })
@@ -13,5 +19,33 @@ Level.create({ name: 'Advanced' })
 Category.create({ name: 'Horse' })
 Category.create({ name: 'Poney' })
 Category.create({ name: 'Double-poney' })
+
+
+Faker::Config.locale = 'fr'
+
+5.times do
+  user = User.new({
+    email: Faker::Internet.email,
+    password: "123456"
+    })
+  user.encrypted_password="ENCRYPT.MY.ASS!!!KJASOPJ090923ULXCIULSH.IXJ!S920"
+  user.save
+end
+
+
+15.times do
+  pic_path = '../../app/assets/images/horses/horse' + rand(1..10).to_s + '.jpg'
+  horse = Horse.new ({
+    name: Faker::Pokemon.name,
+    price: rand(15..100),
+    address: Faker::Address.city,
+    equipment: Faker::Boolean.boolean(0.4),
+    description: Faker::Hipster.sentences(1),
+    photo: src= pic_path,
+    })
+  horse.user = User.find(rand(1..5))
+  horse.category = Category.find(rand(1..3))
+  horse.save
+end
 
 
