b0a49a4bc54077a4993938a2b888e2db57ea7410
Francois Catuhe <fcatuhe@gmail.com>
2016-11-15 22:15:55 +0100

    implemented search logic on horses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   mount Attachinary::Engine => "/attachinary"
+
   root to: 'pages#home'
-  resources :horses, only: [:index, :show, :new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :horses
 end
