6e91079144bdd38b6c451d7f9a28c6687ea5c186
Francois Catuhe <fcatuhe@gmail.com>
2016-11-15 11:39:34 +0100

    added title logic to application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def yield_with_default(holder, default)
+    if content_for?(holder)
+      "#{content_for(holder).squish} | #{default}"
+    else
+      default
+    end
+  end
 end
